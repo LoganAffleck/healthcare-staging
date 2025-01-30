0dae562ade2dd11a376974d541d40174df0fb3d6
J. Logan Affleck <affleck.logan@gmail.com>
2025-01-30 13:37:47 -0500

    Update page.jsx

--- src/app/[...page]/page.jsx
+++ src/app/[...page]/page.jsx
@@ -6,13 +6,14 @@ builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY);
 
 export default async function Page(props) {
   const builderModelName = "page";
+  let params = await props?.params
 
   const content = await builder
     // Get the page content from Builder with the specified options
     .get(builderModelName, {
       userAttributes: {
         // Use the page path specified in the URL to fetch the content
-        urlPath: "/" + (props?.params?.page?.join("/") || ""),
+        urlPath: "/" + (params?.page?.join("/") || ""),
       },
     })
     // Convert the result to a promise
